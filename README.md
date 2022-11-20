3ddb78749fb1a274c67e21f44510d72a8e9d56cc
Alexander Lopez <alexander.lopez@owasp.org>
2022-11-20 16:33:52 -0500

    Include Requirements, Providers, Modules, Resources, Inputs in the ReadMe; Update variable descriptions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# terraform-aws-vpc-module
+# terraform-aws-vpc
 
 # Security & Compliance
 
@@ -6,13 +6,48 @@
 
 # Requirements
 
+| Name | Version |
+| - | - |
+| terraform | >= 1.3.4 |
+| aws | >= 4.39.0 |
+
 # Providers
+
+| Name | Version |
+| - | - |
+| aws | >= 4.39.0 |
+
+
+# Modules
+(None)
 
 # Resources
 
-# Inputs
+| Name | Type |
+| - | - |
+| [aws_vpc](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc) | resource |
 
-# Ouputs
+
+# Inputs
+| Name | Description | Type | Default | Required |
+| - | - | - | - | - |
+| create_vpc | Set to true to enable VPC creation | bool | true | no |
+| use_ipam_pool | Set to true to enable IPAM for CIDR allocation | bool | false | no |
+| ipv4_primary_cidr_block | The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using ipv4_netmask_length | string | null | no |
+| ipv4_ipam_pool_id | The ID of an IPv4 IPAM pool you want to use for allocating this VPC's CIDR. | string | null | no |
+| ipv4_netmask_length | The netmask length of the IPv4 CIDR you want to allocate to this VPC. Requires specifying a ipv4_ipam_pool_id. | number | null | no |
+| enable_ipv6 | Set to true to request an Amazon-provided IPv6 CIDR block with a /56 prefix length | boolean | false | no |
+| ipv6_cidr | IPv6 CIDR block to request from an IPAM Pool. Can be set explicitly or derived from IPAM using ipv6_netmask_length | string | null | no |
+| ipv6_ipam_pool_id |  IPAM Pool ID for a IPv6 pool. Conflicts with assign_generated_ipv6_cidr_block | string | null | no |
+| ipv6_netmask_length | Netmask length to request from IPAM Pool. Conflicts with ipv6_cidr_block. This can be omitted if IPAM pool as a allocation_default_netmask_length set. Valid values: 56 | number | null | no |
+| instance_tenancy | A tenancy option for instances launched into the VPC | string | default | no |
+| enable_dns_hostnames | A boolean flag to enable/disable DNS hostnames in the VPC | boolean | false | no |
+| enable_dns_support | A boolean flag to enable/disable DNS support in the VPC | boolean | true | no |
+| name | Name to be used on all the resources as identifier | string | "" | no |
+| tags | A map of tags to add to all resources | map(string) | {} | no |
+| vpc_tags | Additional tags for the VPC | map(string) | {} | no |
+
+# Outputs
 
 # License
 See LICENSE for full details. 
--- variables.tf
+++ variables.tf
@@ -1,18 +1,9 @@
 variable "create_vpc" {
   type        = bool
   description = <<-EOT
-    Set to `true` to enable VPC creation"
+    Set to `true` to enable VPC creation
   EOT
   default     = true
-}
-
-variable "ipv4_primary_cidr_block" {
-  type        = string
-  description = <<-EOT
-    Define the IPv4 CIDR block for the VPC
-    NOTE: (alopez) This value can also be dervied from IPAM.
-  EOT
-  default     = null
 }
 
 variable "use_ipam_pool" {
@@ -23,10 +14,21 @@ variable "use_ipam_pool" {
   default     = false
 }
 
+variable "ipv4_primary_cidr_block" {
+  type        = string
+  description = <<-EOT
+    (Optional) The IPv4 CIDR block for the VPC. 
+    CIDR can be explicitly set 
+    or it can be derived from IPAM using ipv4_netmask_length
+  EOT
+  default     = null
+}
+
 variable "ipv4_ipam_pool_id" {
   type        = string
   description = <<-EOT
-    Define the ID of an IPv4 IPAM pool to use for CIDR allocation
+    (Optional) The ID of an IPv4 IPAM pool you want 
+    to use for allocating this VPC's CIDR.
   EOT
   default     = null
 }
@@ -34,8 +36,9 @@ variable "ipv4_ipam_pool_id" {
 variable "ipv4_netmask_length" {
   type        = number
   description = <<-EOT
-    Specify the netmask length of the IPv4 CIDR
-    NOTE: (alopez) Use in tandem with ipv4_ipam_pool_id
+    (Optional) The netmask length of the IPv4 CIDR you 
+    want to allocate to this VPC. 
+    Requires specifying a ipv4_ipam_pool_id.
   EOT
   default     = null
 }
@@ -43,25 +46,80 @@ variable "ipv4_netmask_length" {
 variable "enable_ipv6" {
   type        = bool
   description = <<-EOT
-  Set to `true` to request an Amazon-provided IPv6 CIDR block with a /56 prefix length
+    Set to `true` to request an Amazon-provided IPv6 CIDR block with a /56 prefix length
   EOT
   default     = false
 }
 
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
+variable "ipv6_cidr" {
   type        = string
+  description = <<-EOT
+    IPv6 CIDR block to request from an IPAM Pool. 
+    Can be set explicitly or derived from IPAM using 
+    ipv6_netmask_length
+  EOT
+  default     = null
+}
+
+variable "ipv6_ipam_pool_id" {
+  type        = string
+  description = <<-EOT
+    IPAM Pool ID for a IPv6 pool. 
+    Conflicts with assign_generated_ipv6_cidr_block
+  EOT
+  default     = null
+}
+
+variable "ipv6_netmask_length" {
+  type        = number
+  description = <<-EOT
+    Netmask length to request from IPAM Pool. 
+    Conflicts with ipv6_cidr_block. 
+    Valid values: 56
+  EOT
+  default     = null
+}
+
+variable "instance_tenancy" {
+  type        = string
+  description = <<-EOT 
+    A tenancy option for instances launched into the VPC
+  EOT
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = <<-EOT
+   A boolean flag to enable/disable DNS hostnames in the VPC
+  EOT
+  default     = false
+}
+
+variable "enable_dns_support" {
+  type        = bool
+  description = <<-EOT
+    A boolean flag to enable/disable DNS support in the VPC
+  EOT
+  default     = true
+}
+
+variable "name" {
+  type        = string
+  description = <<-EOT
+    Name to be used on all the resources as identifier
+  EOT
   default     = ""
 }
 
-variable "private_subnet_ipv6_prefixes" {
-  description = "Assigns IPv6 private subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
-  type        = list(string)
-  default     = []
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
 }
 
-variable "public_subnet_ipv6_prefixes" {
-  description = "Assigns IPv6 public subnet id based on the Amazon provided /56 prefix base 10 integer (0-256). Must be of equal length to the corresponding IPv4 subnet list"
-  type        = list(string)
-  default     = []
+variable "vpc_tags" {
+  type        = map(string)
+  description = "Additional tags for the VPC"
+  default     = {}
 }
